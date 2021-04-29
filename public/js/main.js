4279d23bc31f04da774c58ed7719b746103d3c9f
t-awka <80962656+t-awka@users.noreply.github.com>
2021-04-29 15:15:50 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+// exo1
+function envers(lenombre) {
+    return console.log(lenombre.toString().split('').reverse().join(''));
+}
+envers (1865);
+
+//exo2
+// function divPar2(number) {
+//     switch (number % 2) {
+//         case 0:
+//             return console.log(`Le ${number} est divisible par 2. ${number}:2 = ${number/2}`);
+//         case 1:
+//             return console.log("Ce nombre n'est pas divisible par 2");
+//         default:
+//             return console.log("ceci n'est pas un nombre");
+//     }
+// }
+
+// divPar2(8);
+
+//exo3
+// function logIn() {
+//     let mdp;
+//     while (mdp != "mdp") {
+//         mdp = prompt('Votre mot de passe :');
+//     }
+//     alert("Bravo t'es co maggle");
+// }
+
+// logIn();
